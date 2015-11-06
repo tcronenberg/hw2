b050ef9920616cded90ef287c639791af530866f
Tom Cronenberg <tom.cronenberg@hhu.de>
2015-11-06 13:05:48 +0100

    Results are now written to a file

--- logmap.cxx
+++ logmap.cxx
@@ -1,12 +1,18 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main(){
+  
 	double x0 = 0.5;
 	double x;
 	int Nskip = 100; //Number of iterations to skip
 	int Nend  = 200; //Number of total iterations
+	
+	double xarr[Nend-Nskip];
+	
+	ofstream out("output.dat");
 
 	for(double r=0; r <= 4; r += 0.001){
 	   x=x0;
@@ -14,10 +20,13 @@ int main(){
 		   x = r*x*(1-x);
 	   for(int i=Nskip+1; i <= Nend; i++){
 	   		   x = r*x*(1-x);
-	   		   cout << r << "\t" << x << endl;
+	   		   //cout << r << "\t" << x << endl;
+			   xarr[i-Nskip+1] = x;
    	   }
+   	   for(int i=Nskip+1; i <= Nend; i++)
+			   out << i << "\t" << xarr[i-Nskip+1] << "\t" << r << endl;
 	}
 
-
+	out.close();
 	return 0;
 }
